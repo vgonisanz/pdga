f7bff50750b190a34d11a892f189dc46908cd427
vgonisanz <vgonisanz@gmail.com>
2020-08-07 23:02:17 +0200

    isolate: draft of an algorithm, need to work more

--- pdga/samples/04_isolate_cells.py
+++ pdga/samples/04_isolate_cells.py
@@ -12,16 +12,41 @@ wwidth = 800
 wheight = 600
 initx = wwidth/8*3
 inity = wheight/8*3
-width = wwidth/4
-height = wheight/4
+width = wwidth/8
+height = wheight/8
 grid_size = 10
 
 cells = []
 
+def isolate_cells(cell0, cell1):
+    # Move X axi
+    xsubtract = cell1.rect.left - cell0.rect.left
+
+    if xsubtract > 0:
+        cell1.move_right()
+    elif xsubtract < 0:
+        cell0.move_left()
+    # Move Y axi
+    ysubtract = cell1.rect.top - cell0.rect.top
+
+    if ysubtract > 0:
+        cell1.move_down()
+    elif ysubtract < 0:
+        cell0.move_up()
+
 def draw():
     for cell in cells:
         engine.draw_cell(cell)
-
+    # Idea, buscar el más alejado, y terminar de alejarlo
+    # primero X y luego Y
+    print("Update physics")
+    for x in range(len(cells) - 1):
+        for y in range(x + 1, len(cells)):
+            print(f"combine {x} and {y}")
+            cell0 = cells[x]
+            cell1 = cells[y]
+            if cell0.rect.colliderect(cell1.rect):
+                isolate_cells(cell0=cell0, cell1=cell1)
     time.sleep(0.05)
 
 def events(event):
@@ -30,18 +55,11 @@ def events(event):
     
 def init():
     global cells
+ 
+    for x in range(10):
+        cell = Generators.cell_random(initx, inity, width, height, 3, grid_size)
+        cells.append(cell)
 
-    cell1 = Cell(initx, inity, width, height,
-        bg_color=Colors.MidnightBlue,
-        grid_color=Colors.LightSkyBlue,
-        border_color=Colors.White,
-        grid_width=grid_size,
-        grid_height=grid_size
-    )
-
-    cell2 = Generators.cell_random(initx, inity, width, height, 3, grid_size)
-
-    cells = [ cell1, cell2 ]
 
 if __name__ == "__main__":
     print(f"Initializing engine with {wwidth}, {wheight}")
